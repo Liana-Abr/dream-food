bee888b874b8251f0a99faa31b034a57832bab9e
Liana-Abr <AbramyanLV20@st.ithub.ru>
2021-11-19 11:45:07 +0300

    food

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("<h1>Dream Food</h1>")
+});
+app.listen(port);
