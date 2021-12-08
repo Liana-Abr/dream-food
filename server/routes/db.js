2c1e976e7db610e249281f013bc718f50688e6ec
Liana-Abr <AbramyanLV20@st.ithub.ru>
2021-12-08 17:07:16 +0300

    n

--- server/routes/db.js
+++ server/routes/db.js
@@ -1,6 +1,8 @@
 const connectDB = function(){
     const MongoClient = require("mongodb").MongoClient;
-    const uri = "mongodb+srv://Liana:<password>@cluster0.zfsq6.mongodb.net/food?retryWrites=true&w=majority"
+    const user = process.env.DBNAME || "Liana";
+    const pwd = process.env.DBPASS || "12345";
+    const uri = `mongodb+srv://${user}:${pwd}@cluster0.zfsq6.mongodb.net/food?retryWrites=true&w=majority`
     return new MongoClient(uri,{
         useNewUrlParser: true,
         useUnifiedTopology: true
